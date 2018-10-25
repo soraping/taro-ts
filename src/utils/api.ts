722dd2ab4aea1d9711a1404a2603a49d91ccdcd1
caoshiping <caoshiping@qianmi.com>
2018-10-25 16:15:27 +0800

    add

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import { Http } from "./request";
 import * as API from "../constants/api";
-import { IMiniLogin, ICat } from "../interface";
+import { IMiniLogin } from "../interface";
 const http = new Http();
 
 /**
@@ -11,12 +11,3 @@ export const userLogin = (code: string) =>
   http.post<IMiniLogin>(API.LOGIN, {
     code
   });
-
-/**
- * 获取猫列表
- */
-export const catList = () => http.get<ICat[]>(API.CAT_LIST);
-/**
- * 添加猫
- */
-export const catAdd = (params: ICat) => http.post<any>(API.CAT_ADD, params);
